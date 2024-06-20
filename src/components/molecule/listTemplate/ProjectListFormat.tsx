6238f08fd05e28fd1778dbba78096f93877f8e6d
yusuken1121 <yusuken1121@gmail.com>
2024-06-19 23:59:10 -0700

    ⚡️ Add useMemo

--- src/components/molecule/listTemplate/ProjectListFormat.tsx
+++ src/components/molecule/listTemplate/ProjectListFormat.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useContext } from "react";
+import React, { FC, ReactNode, useContext } from "react";
 import { ProjectItemFormat } from "../itemTemplate/ProjectItemFormat";
 import { ProjectsCtx } from "../../../provider/ProjectCtx";
 import { Project, ProjectListType } from "../../../types/project";
@@ -10,47 +10,46 @@ type ProjectListFormat = {
   projectType: ProjectListType;
 };
 
-export const ProjectListFormat: FC<ProjectListFormat> = ({
-  children,
-  projectType,
-}) => {
-  const { projects } = useContext(ProjectsCtx);
-  const { setNodeRef } = useDroppable({ id: projectType });
+export const ProjectListFormat: FC<ProjectListFormat> = React.memo(
+  ({ children, projectType }) => {
+    const { projects } = useContext(ProjectsCtx);
+    const { setNodeRef } = useDroppable({ id: projectType });
 
-  const filteredProjects = projects.filter(
-    (project: Project) => project.type === projectType
-  );
+    const filteredProjects = projects.filter(
+      (project: Project) => project.type === projectType
+    );
 
-  // Check for the range of the dropzone
-  // const dropZoneStyle = {
-  //   border: isOver ? "2px dashed red" : "2px solid skyblue",
-  //   backgroundColor: isOver ? "#e0ffe0" : "transparent",
-  // };
+    // Check for the range of the dropzone
+    // const dropZoneStyle = {
+    //   border: isOver ? "2px dashed red" : "2px solid skyblue",
+    //   backgroundColor: isOver ? "#e0ffe0" : "transparent",
+    // };
 
-  return (
-    <div className="project-list-base" ref={setNodeRef}>
-      <div className="banner-base flex justify-center">
-        <h1 className="h1-base mb-2">{children}</h1>
+    return (
+      <div className="project-list-base" ref={setNodeRef}>
+        <div className="banner-base flex justify-center">
+          <h1 className="h1-base mb-2">{children}</h1>
+        </div>
+        <SortableContext
+          id={projectType}
+          items={filteredProjects.map((project: Project) => project.id)}
+          strategy={rectSortingStrategy}
+        >
+          <ul className="w-full flex flex-col justify-start items-center">
+            {filteredProjects.map((project: Project) => {
+              return (
+                <ProjectItemFormat
+                  key={project.id}
+                  id={project.id}
+                  title={project.title}
+                  description={project.description}
+                  people={project.people}
+                />
+              );
+            })}
+          </ul>
+        </SortableContext>
       </div>
-      <SortableContext
-        id={projectType}
-        items={filteredProjects.map((project: Project) => project.id)}
-        strategy={rectSortingStrategy}
-      >
-        <ul className="w-full flex flex-col justify-start items-center">
-          {filteredProjects.map((project: Project) => {
-            return (
-              <ProjectItemFormat
-                key={project.id}
-                id={project.id}
-                title={project.title}
-                description={project.description}
-                people={project.people}
-              />
-            );
-          })}
-        </ul>
-      </SortableContext>
-    </div>
-  );
-};
+    );
+  }
+);
