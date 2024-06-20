09d87273dfe2c56c8ca3efae9c202b62e25c9d93
yusuken1121 <yusuken1121@gmail.com>
2024-06-20 00:03:58 -0700

    ⚡️ Add React.memo

--- src/components/molecule/itemTemplate/ProjectItemFormat.tsx
+++ src/components/molecule/itemTemplate/ProjectItemFormat.tsx
@@ -1,5 +1,5 @@
 import { useSortable } from "@dnd-kit/sortable";
-import { FC } from "react";
+import React, { FC } from "react";
 import { CSS } from "@dnd-kit/utilities";
 import { IoAccessibility } from "react-icons/io5";
 import { IoChatbubbleEllipses } from "react-icons/io5";
@@ -12,38 +12,35 @@ type ProjectItemFormatType = {
   people: number;
 };
 
-export const ProjectItemFormat: FC<ProjectItemFormatType> = ({
-  id,
-  title,
-  description,
-  people,
-}) => {
-  const { attributes, listeners, setNodeRef, transform } = useSortable({
-    id: id,
-  });
-  const style = {
-    transform: CSS.Transform.toString(transform),
-  };
-  return (
-    <li
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      className="project-item-base"
-    >
-      <p className="item-container-base text-xl md:text-2xl">
-        <IoListCircle className="item-icons-base" />
-        {title}
-      </p>
-      <p className="item-container-base text-xl">
-        <IoChatbubbleEllipses className="item-icons-base" />
-        {description}
-      </p>
-      <p className="item-container-base text-xl">
-        <IoAccessibility className="item-icons-base" />
-        {people}
-      </p>
-    </li>
-  );
-};
+export const ProjectItemFormat: FC<ProjectItemFormatType> = React.memo(
+  ({ id, title, description, people }) => {
+    const { attributes, listeners, setNodeRef, transform } = useSortable({
+      id: id,
+    });
+    const style = {
+      transform: CSS.Transform.toString(transform),
+    };
+    return (
+      <li
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}
+        className="project-item-base"
+      >
+        <p className="item-container-base text-xl md:text-2xl">
+          <IoListCircle className="item-icons-base" />
+          {title}
+        </p>
+        <p className="item-container-base text-xl">
+          <IoChatbubbleEllipses className="item-icons-base" />
+          {description}
+        </p>
+        <p className="item-container-base text-xl">
+          <IoAccessibility className="item-icons-base" />
+          {people}
+        </p>
+      </li>
+    );
+  }
+);
