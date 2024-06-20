02135e272bae74fdfaa48920e94b503c97f203fd
yusuken1121 <yusuken1121@gmail.com>
2024-06-19 19:41:39 -0700

    💄 Fix the H1 position on Header

--- src/components/organism/header/Header.tsx
+++ src/components/organism/header/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header className="banner-base">
-      <h1 className="h1-base">PROJECTS MANAGEMENT</h1>
+      <h1 className="h1-base ml-5">PROJECTS MANAGEMENT</h1>
     </header>
   );
 };
