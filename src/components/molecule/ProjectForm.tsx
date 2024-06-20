ebd89c8feec37ad8380e319b7e5a815105aaacd8
yusuken1121 <yusuken1121@gmail.com>
2024-06-19 23:38:29 -0700

    ⚡️ Add useCallBack

--- src/components/molecule/ProjectForm.tsx
+++ src/components/molecule/ProjectForm.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import { SubmitButton } from "../atom/SubmitButton";
 import { v4 as uuid } from "uuid";
 import { ProjectsCtx } from "../../provider/ProjectCtx";
@@ -11,64 +11,74 @@ export const ProjectForm = () => {
   const [people, setPeople] = useState<number>(1);
   const { projects, setProjects } = useContext(ProjectsCtx);
 
-  const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value);
-  };
-  const handleChangeDescription = (
-    e: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setDescription(e.target.value);
-  };
-  const handlePeopleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPeople(parseInt(e.target.value));
-  };
-  const submitProject = (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleChangeTitle = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTitle(e.target.value);
+    },
+    []
+  );
+  const handleChangeDescription = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setDescription(e.target.value);
+    },
+    []
+  );
+  const handlePeopleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPeople(parseInt(e.target.value));
+    },
+    []
+  );
+  const submitProject = useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
 
-    const titleValidatable: Validatable = {
-      value: title,
-      required: true,
-      minLength: 1,
-      maxLength: 20,
-    };
+      const titleValidatable: Validatable = {
+        value: title,
+        required: true,
+        minLength: 1,
+        maxLength: 20,
+      };
 
-    const descriptionValidatable: Validatable = {
-      value: description,
-      required: true,
-      minLength: 1,
-      maxLength: 20,
-    };
+      const descriptionValidatable: Validatable = {
+        value: description,
+        required: true,
+        minLength: 1,
+        maxLength: 20,
+      };
 
-    const peopleValidatable: Validatable = {
-      value: people,
-      required: true,
-      min: 1,
-      max: 10,
-    };
+      const peopleValidatable: Validatable = {
+        value: people,
+        required: true,
+        min: 1,
+        max: 10,
+      };
 
-    if (
-      !validate(titleValidatable) ||
-      !validate(descriptionValidatable) ||
-      !validate(peopleValidatable)
-    ) {
-      alert("INVALID INPUT DATA");
-      return;
-    }
-    setProjects([
-      ...projects,
-      {
-        id: uuid(),
-        title,
-        description,
-        people,
-        type: ProjectListType.ACTIVE,
-      },
-    ]);
+      if (
+        !validate(titleValidatable) ||
+        !validate(descriptionValidatable) ||
+        !validate(peopleValidatable)
+      ) {
+        alert("INVALID INPUT DATA");
+        return;
+      }
+      setProjects([
+        ...projects,
+        {
+          id: uuid(),
+          title,
+          description,
+          people,
+          type: ProjectListType.ACTIVE,
+        },
+      ]);
 
-    setTitle("");
-    setDescription("");
-    setPeople(1);
-  };
+      setTitle("");
+      setDescription("");
+      setPeople(1);
+    },
+    [title, description, people, setProjects, projects]
+  );
 
   return (
     <form className="project-form-base">
